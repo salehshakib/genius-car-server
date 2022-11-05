ad4251b5cd41e599071ff68fb4e973d2d7c6cc05
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2022-11-05 16:48:26 +0600

    mongodb data added

--- index.js
+++ index.js
@@ -10,9 +10,41 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+// const user = 'salehshakib'
+// const password = 'kvUlqGh6vhajT2aR';
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mycluster0.23iyqhu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const database = client.db('geniuscar')
+        const serviceCollection = database.collection('services');
+
+        const orderCollection = client.db('geniuscar').collection('orders');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('genius car server is running')
